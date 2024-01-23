2f0a849a99fb78f74cbbb597aef6974887041f77
CaitlynChavez <caitlynchavez@outlook.com>
2024-01-23 00:53:38 -0800

    fixing the function to create dataframe

--- src/explainbrainroi/pages/2-Process_Data.py
+++ src/explainbrainroi/pages/2-Process_Data.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import subprocess
 import os 
+import pandas as pd
 
 # Custom 
 # from utils.FSL_Processing import (invwarp, applywarp_cort, applywarp_subcort, apply_warps)
@@ -10,8 +11,6 @@ from utils.FSL_Processing import (invwarp, applywarp_cort, applywarp_subcort)
 # -----------------------------------------------------------------------------------------------------------------------------
 # ENVIRONMENT SETUP
 # -----------------------------------------------------------------------------------------------------------------------------
-
-
 
 
 
@@ -63,18 +62,20 @@ st.markdown(
 
 # st.session_state["loaded_images"] = loaded_images
 disable_button=False
-if st.button("Run FSL_anat", disabled=disable_button):
+if st.button("Run FSL_anat Pipeline", disabled=disable_button):
 
     st.write(["fsl_anat","-i", st.session_state["uploaded_image_filepath"]], capture_output=True)
 
 
-    # process = subprocess.run(["fsl_anat", "-i", str(st.session_state["uploaded_image_filepath"])], capture_output=True)
-    # st.session_state["image_anat_output"] = process
+    process = subprocess.run(["fsl_anat", "-i", str(st.session_state["uploaded_image_filepath"])], capture_output=True)
+    st.session_state["image_anat_output"] = process
 
     # st.write("Output:", process.stdout)
     # st.write("Error:", process.stderr)
     st.write("### Preprocessing using FSL_anat Completed")
     disable_button=True
+
+
 
 st.write("### Applying Warps Pipeline Using FSL")
 
@@ -97,6 +98,12 @@ st.markdown(
 
 
 # Here we are running the full pipeline for the warping process.  This will take approximately 40 minutes to run per image. 
+
+
+root, ext = os.path.splitext(st.session_state["uploaded_image_filepath"])
+st.session_state["root_path"] = root
+
+
 disable_button=False
 if st.button("Warping Pipeline", disabled=disable_button):
     # st.write("### Applying Warps 1")
@@ -123,6 +130,98 @@ if st.button("Warping Pipeline", disabled=disable_button):
     time.sleep(3)  # Sleep for 3 seconds to ensure completion before the next step
 
     st.write("### Applying Warps Pipeline Using FSL Completed")
+
+
+#Building the dataframe for the model
+    
+st.markdown(
+    """
+    ### To collect the volume data we will build a dataframe and will also join the original data wherein contains the labels, age, and sex of the patient.  
+
+    """
+)
+
+disable_button=False
+if st.button("Building Dataframe ", disabled=disable_button):
+    
+    # Cortical FSL Stats Volume Extraction
+    st.session_state["patient_anat"] = st.session_state["root_path"]+".anat"
+    # st.write("Hi cait", st.session_state["patient_anat"]+"/Warps/HO_in_subj_t1_space.nii.gz")
+    # st.write("Hi cait2", st.session_state["patient_anat"]+"/T1.nii.gz")
+    out_cort = subprocess.Popen(["fslstats", "-K", st.session_state["patient_anat"]+"/Warps/HO_in_subj_t1_space.nii.gz", st.session_state["patient_anat"]+"/T1.nii.gz", "-V"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout_cort, stderr_cort = out_cort.communicate()
+    st.session_state["cort_fsl_volumes"] = stdout_cort
+    st.write(stdout_cort)
+
+    # for i in st.session_state["fsl_volumes"]:
+    #     st.write(i)
+
+    # vols[str(i)] = [float(str(stdout_subcort.split()[0])[2:-1]), float(str(stdout_subcort.split()[1])[2:-1])]
+    st.write("NEW***************************************************************************************")
+
+
+    # Subcortical FSL Stats Volume Extraction
+    # st.session_state["patient_anat"] = st.session_state["root_path"]+".anat"
+    # st.write("Hi cait", st.session_state["patient_anat"]+"/Warps/subcort_HO_in_subj_t1_space.nii.gz")
+    # st.write("Hi cait2", st.session_state["patient_anat"]+"/T1.nii.gz")
+    # out_subcort = sout = subprocess.Popen(["fslstats", "-K", st.session_state["patient_anat"]+"/Warps/subcort_HO_in_subj_t1_space.nii.gz", st.session_state["patient_anat"]+"/T1.nii.gz", "-V"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT) # CAIT but this one works
+    process = sout = subprocess.Popen(["fslstats", "-K", st.session_state["patient_anat"]+"/Warps/subcort_HO_in_subj_t1_space.nii.gz", st.session_state["patient_anat"]+"/T1.nii.gz", "-V"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, subcort_stderr = process.communicate()
+    st.session_state["fsl_volumes"] = stdout
+    st.write(stdout)
+
+    # vols[str(i)] = [float(str(stdout_subcort.split()[0])[2:-1]), float(str(stdout_subcort.split()[1])[2:-1])]
+    st.write("NEW***************************************************************************************")
+
+
+    # subcort_output = out_subcort.stdout_subcort.strip()
+    output = process.stdout.strip()
+
+    # Split the output into mean intensity and volume
+    mean_intensity, volume_info = subcort_output.split()
+    volume_voxels, volume_mm3 = volume_info.split(";")
+
+    # Create a DataFrame
+    df = pd.DataFrame({
+        "Mean Intensity": [float(mean_intensity)],
+        "Volume (Voxels)": [int(volume_voxels)],
+        "Volume (mm³)": [float(volume_mm3)]
+    })
+
+# st.write(df)
+
+
+
+
+
+# import subprocess
+# import pandas as pd
+
+# def create_dataframe_from_fslstats(fsl_command):
+#     # Run the fslstats command
+#     process = subprocess.run(fsl_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+#     # Capture the output
+#     output = process.stdout.strip()
+
+#     # Split the output into mean intensity and volume
+#     mean_intensity, volume_info = output.split()
+#     volume_voxels, volume_mm3 = volume_info.split(";")
+
+#     # Create a DataFrame
+#     df = pd.DataFrame({
+#         "Mean Intensity": [float(mean_intensity)],
+#         "Volume (Voxels)": [int(volume_voxels)],
+#         "Volume (mm³)": [float(volume_mm3)]
+#     })
+
+#     return df
+
+# # Example usage
+# fsl_command = "fslstats -K ./sub-A00000844_ses-20100101_acq-mprage_echo-01_T1w.anat/Warps/subcort_HO_in_subj_t1_space.nii.gz ./sub-A00000844_ses-20100101_acq-mprage_echo-01_T1w.anat/T1.nii.gz -M -V"
+# df = create_dataframe_from_fslstats(fsl_command)
+# print(df)
+
 
 
 
